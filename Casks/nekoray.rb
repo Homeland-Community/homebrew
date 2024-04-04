8697d60ceec95c31b3eaab6f976c49d5b6ef9d53
Sosha Mosleh <sosha1996@riseup.net>
2024-04-04 00:02:45 +0000

    add app arch

--- Casks/nekoray.rb
+++ Casks/nekoray.rb
@@ -1,8 +1,11 @@
 cask 'nekoray' do
     version "3.26"
-    sha256 "4F127B99F12A97E070528AD73C52C4CBC70E7929B02B9F7A5B3755B07C2F7DF2"
+    arch arm: "arm64",
+        intel: "x86_64"
+    sha256 arm: "4F127B99F12A97E070528AD73C52C4CBC70E7929B02B9F7A5B3755B07C2F7DF2",
+        intel: "B57080209221658B1A086EE9C01FCA7EDB395E2674261E8E9F8E87EED6387614"
 
-    url "https://github.com/abbasnaqdi/nekoray-macos/releases/download/#{version}/nekoray_arm64.zip",
+    url "https://github.com/abbasnaqdi/nekoray-macos/releases/download/#{version}/nekoray_#{arch}.zip",
         verified: "github.com/abbasnaqdi/nekoray-macos"
     name "nekoray-macos"
     desc "This script is made to build https://github.com/MatsuriDayo/nekoray software for use on macOS operating system."
@@ -10,5 +13,5 @@ cask 'nekoray' do
   
     auto_updates true
 
-    app "nekoray_arm64.app"
+    app "nekoray_#{arch}.app"
 end
